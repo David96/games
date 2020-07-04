b64b10c2d964665817b2a926c6b2f3e8c03b5c84
David96 <david@hameipe.de>
2020-07-04 17:46:30 +0200

    Some refactoring

--- game_room.py
+++ game_room.py
@@ -20,14 +20,21 @@ class GameRoom:
     async def send_error(self, socket, message, error_type=''):
         await socket.send(json.dumps({'type': 'error', 'error': error_type, 'msg': message}))
 
-    async def send_message(self, message):
+    def send_message(self, message):
         if self.users:
             msg = json.dumps({'type':'message', 'msg': message})
-            await asyncio.wait([socket.send(msg) for socket in self.users.values()])
+            asyncio.ensure_future(asyncio.wait(
+                [socket.send(msg) for socket in self.users.values()]))
+
+    def call_later(self, delay, callback):
+        def run_scheduled():
+            callback()
+            asyncio.ensure_future(self.game.send_dirty())
+        asyncio.get_event_loop().call_later(delay, run_scheduled)
 
     async def join(self, name, socket):
         if name in self.users:
-            return False
+            raise Exception('Name already taken!')
         if not self.users:
             self.creator = name
             await socket.send(json.dumps({'type': 'rights', 'status': 'creator'}))
@@ -35,17 +42,19 @@ class GameRoom:
         self.users[name] = socket
         if not await self.game.add_player(name):
             del self.users[name]
-            return False
-        await self.send_message('%s joined the game!' % name)
-        return True
+            raise Exception('Game already running.')
+        self.send_message('%s joined the game!' % name)
+        await socket.send(json.dumps({'type': 'joined'}))
+        await self.game.send_dirty()
 
     async def leave(self, name):
         del self.users[name]
         if self.creator == name and self.users:
             self.creator = list(self.users.keys())[0]
             await self.users[self.creator].send(json.dumps({'type': 'rights', 'status': 'creator'}))
-        await self.game.remove_player(name)
-        await self.send_message('%s left the game!' % name)
+        self.game.remove_player(name)
+        await self.game.send_dirty()
+        self.send_message('%s left the game!' % name)
 
         # Reset game if all users are gone so newly joined users don't see the old score board
         if not self.users:
@@ -54,33 +63,20 @@ class GameRoom:
 
     async def fire_event(self, sender_name, event):
         if not event.per_user or not event.notify_all:
-            data = await event.event(sender_name)
+            data = event.event(sender_name)
         if event.notify_all:
             for name, socket in self.users.items():
                 if event.per_user:
-                    data = await event.event(name)
-                print(data)
+                    data = event.event(name)
                 if data:
                     await socket.send(data)
         else:
-            print(data)
             if data:
                 await self.users[sender_name].send(data)
 
-    async def run_action(self, action, name, data, socket):
-        try:
-            await action(name, data)
-        except Exception as e:
-            await self.send_error(socket, str(e))
-            traceback.print_exc()
-
     async def on_message(self, name, message):
         socket = self.users[name]
-        try:
-            data = json.loads(message)
-        except Exception as e:
-            await self.send_error(socket, str(e))
-            return
+        data = json.loads(message)
         if not self.started or self.game.game_over:
             if data['action'] != 'start_game':
                 await self.send_error(socket, 'You can\'t do shit without starting the game first!')
@@ -88,10 +84,12 @@ class GameRoom:
                 await self.send_error(socket, 'That\'s not your job to do!')
             else:
                 self.started = True
-                await self.game.start_game()
+                self.game.start_game()
+                await self.game.send_dirty()
         elif data['action'] in self.game.ACTIONS:
             action = self.game.ACTIONS[data['action']]
-            asyncio.ensure_future(self.run_action(action, name, data, socket))
+            action(name, data)
+            await self.game.send_dirty()
         else:
             await self.send_error(socket, '%s is not a valid action!' % data['action'],
                                   'invalid_action')
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import websockets
+import traceback
 
 from game_room import GameRoom
 from wizard.wizard import Wizard
@@ -11,18 +12,24 @@ async def serve(websocket, path):
     name = None
     try:
         async for message in websocket:
-            data = json.loads(message)
-            if 'action' in data and data['action'] == 'join':
-                name = data['name']
-                if not name:
-                    await wizard.send_error(websocket, 'Name must not be empty!', 'empty_name')
-                elif not await wizard.join(name, websocket):
-                    await wizard.send_error(websocket,
-                            "Name already taken or game currently running.", "name_taken")
-            elif name:
-                await wizard.on_message(name, message)
+            try:
+                data = json.loads(message)
+                if 'action' in data and data['action'] == 'join':
+                    name = data['name']
+                    if not name:
+                        await wizard.send_error(websocket, 'Name must not be empty!', 'empty_name')
+                    else:
+                        await wizard.join(name, websocket)
+                elif name:
+                    await wizard.on_message(name, message)
+            except json.JSONDecodeError as e:
+                await wizard.send_error(websocket, 'Malformed message! Ignoring.')
+            except Exception as e:
+                await wizard.send_error(websocket, str(e))
+                traceback.print_exc()
     finally:
-        await wizard.leave(name)
+        if name:
+            await wizard.leave(name)
 
 
 start_server = websockets.serve(serve, "localhost", 6791)
